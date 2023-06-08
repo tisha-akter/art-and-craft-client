90b111c43eb9c5a9f6b9c1d2e0b1d967d002d939
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-08 20:13:38 +0600

    added popular classes based on number of students

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import PopularClass from "../PopularClass/PopularClass";
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <PopularClass></PopularClass>
         </div>
     );
 };
---
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+
+const PopularClass = () => {
+
+    const [popularClass, setPopularClass] = useState([]);
+    useEffect(() => {
+        fetch('popularclass.json')
+            .then(res => res.json())
+            .then((data) => {
+                // Sort the classes based on the number of students
+                const sortedClasses = data.sort((a, b) => b.number_of_students - a.number_of_students);
+
+                const topClasses = sortedClasses.slice(0, 6);
+                setPopularClass(topClasses);
+            })
+            .catch((error) => {
+                console.error('Error fetching data from JSON:', error);
+            });
+    }, [])
+
+
+    return (
+        <div className="my-5">
+            <div className=' text-center font-light text-gray-500 sm:text-lg dark:text-gray-400 py-5'>
+                <h1 className="mb-4 text-4xl tracking-tight font-extrabold font-serif text-gray-900 dark:text-white">Popular <span className="text-violet-600">Class</span></h1>
+                <p className="mb-5">Some of our popular classes based on the number of students.</p>
+            </div>
+
+            <div className="grid md:grid-cols-3 gap-10">
+
+                {popularClass.map(classData => (
+                    <div className="card w-96 bg-base-100 shadow-xl" key={classData._id}>
+                        <figure className="px-10 pt-10">
+                            <img src={classData.image} alt="Shoes" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="text-center text-2xl font-serif text-violet-500 font-bold">{classData.class_name}</h2>
+                            <p className="text-gray-500">{classData.description}</p>
+                            
+                                <h5 className="text-xl  text-gray-600">Number of students: {classData.number_of_students}</h5>
+                                <h5 className="text-xl text-gray-600 ">Available seats: {classData.available_seats}</h5>
+                                <h5 className="text-xl  text-gray-600">Cost : {classData.price}$</h5>
+
+                            <div className="my-3 text-center items-center">
+                                <button className="btn btn-primary btn-sm bg-violet-500 ">Enroll now!!</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+
+        </div>
+    );
+};
+
+export default PopularClass;
