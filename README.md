08bfe59c9aa036d512b4bc1131bfbc0d7908c7dd
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-13 18:40:05 +0600

    added readme.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+...
+
+<!-- # [live site]() -->
+
+
+# Art & Craft (Summer Camp School Project)
+
+Welcome to the Art and Craft Website, a summer camp school-related platform that aims to inspire creativity and foster artistic skills in children. This repository contains the source code for the website.
+
+## Technologies Used
+
+The Art and Craft Website utilizes the following technologies:
+
+* JavaScript: The primary programming language used for the website's functionality.
+* MongoDB: A NoSQL database used to store and manage data.
+* Firebase: A platform that provides various backend services, including authentication and hosting.
+* Framer Motion: A React library for creating smooth and interactive animations.
+* TanStack Query (React Query): A powerful data-fetching and state management library for React applications.
+* React Icons: A library of customizable icons for React applications.
+* Axios: A library for making HTTP requests from the browser.
+* Custom Hooks: Custom reusable hooks for managing state and logic.
--- src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,38 +1,110 @@
+import Swal from "sweetalert2";
 import useClassesInfo from "../../../hooks/useClassesInfo";
+import { useState } from "react";
 
 const ManageClasses = () => {
+    const [classesInfo, setClassesInfo] = useClassesInfo();
+    const [loading, setLoading] = useState(false);
 
-    const [classesInfo] = useClassesInfo();
+    const handleApprove = async (classId) => {
+        setLoading(true);
+        try {
+            // Send a PATCH request to update the class status as "approved" in the backend
+            await fetch(`http://localhost:5000/classesInfo/${classId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ status: "approved" }),
+            });
 
+            // Update the class status in the local state
+            const updatedClasses = classesInfo.map((item) => {
+                if (item._id === classId) {
+                    return { ...item, status: "approved" };
+                }
+                return item;
+            });
+            setClassesInfo(updatedClasses);
+        } catch (error) {
+            console.error("Error approving class:", error);
+        }
+        setLoading(false);
+    };
+
+    const handleDeny = async (classId) => {
+        setLoading(true);
+        try {
+            // Send a PATCH request to update the class status as "denied" in the backend
+            await fetch(`http://localhost:5000/classesInfo/${classId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ status: "denied" }),
+            });
+
+            // Update the class status in the local state
+            const updatedClasses = classesInfo.map((item) => {
+                if (item._id === classId) {
+                    return { ...item, status: "denied" };
+                }
+                return item;
+            });
+            setClassesInfo(updatedClasses);
+        } catch (error) {
+            console.error("Error denying class:", error);
+        }
+        setLoading(false);
+    };
+
+    const handleSendFeedback = (classId) => {
+        Swal.fire({
+            title: "Send Feedback",
+            text: "Enter your feedback:",
+            input: "textarea",
+            showCancelButton: true,
+            confirmButtonText: "Send",
+            cancelButtonText: "Cancel",
+            preConfirm: (feedback) => {
+                // Handle feedback submission here
+                console.log("Feedback:", feedback);
+                // You can send the feedback and classId to the server or perform any other necessary actions
+                console.log("Class ID:", classId);
+            },
+            allowOutsideClick: () => !Swal.isLoading(),
+            showLoaderOnConfirm: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return "Feedback is required!";
+                }
+            },
+        });
+    };
 
 
     return (
         <div className="w-full">
-
             <div className="overflow-x-auto mt-20">
                 <table className="table">
                     {/* head */}
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th> Class Image </th>
+                            <th>Class Image</th>
                             <th>Class name</th>
                             <th>Instructor name</th>
-                            <th> Instructor email</th>
+                            <th>Instructor email</th>
                             <th>Available seats</th>
-                            <th> Price</th>
+                            <th>Price</th>
                             <th>Status</th>
                             <th>Action</th>
-
                         </tr>
                     </thead>
                     <tbody>
-
-                        {
-                            classesInfo.map((item, index) => <tr key={item._id}>
-                                <td>
-                                    {index + 1}
-                                </td>
+                        {classesInfo.map((item, index) => (
+                            <tr key={item._id}>
+                                <td>{index + 1}</td>
                                 <td>
                                     <div className="flex items-center space-x-3">
                                         <div className="avatar">
@@ -42,7 +114,6 @@ const ManageClasses = () => {
                                         </div>
                                     </div>
                                 </td>
-
                                 <td>
                                     <div>
                                         <div>{item.class_name}</div>
@@ -58,26 +129,45 @@ const ManageClasses = () => {
                                         <div>{item.email}</div>
                                     </div>
                                 </td>
-
-                                <td className=""><div>{item.available_seats}</div></td>
-                                <td className=""><div>{item.price} $</div></td>
-                               
-                               <td>
-                                <div>
-                                {item.status}
-                                </div>
-                               </td>
-
+                                <td className="">
+                                    <div>{item.available_seats}</div>
+                                </td>
+                                <td className="">
+                                    <div>{item.price} $</div>
+                                </td>
+                                <td>
+                                    <div>{item.status}</div>
+                                </td>
                                 <td>
                                     <div className="">
-                                        <button className="btn btn-xs mb-1"> Approve</button>
-                                        <button className="btn btn-xs mb-1">Deny</button>
-                                        <button className="btn btn-xs mb-1">feedback </button>
+                                        {item.status === "pending" && (
+                                            <>
+                                                <button
+                                                    className="btn btn-xs mb-1"
+                                                    disabled={loading}
+                                                    onClick={() => handleApprove(item._id)}
+                                                >
+                                                    Approve
+                                                </button>
+                                                <button
+                                                    className="btn btn-xs mb-1"
+                                                    disabled={loading}
+                                                    onClick={() => handleDeny(item._id)}
+                                                >
+                                                    Deny
+                                                </button>
+                                            </>
+                                        )}
+                                        <button
+                                            className="btn btn-xs mb-1"
+                                            onClick={() => handleSendFeedback(item._id)}
+                                        >
+                                            Feedback
+                                        </button>
                                     </div>
                                 </td>
-
-                            </tr>)
-                        }
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
